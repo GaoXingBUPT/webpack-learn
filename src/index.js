f5d52b86d0fd2460a7a170c3fd444ccb0ebfc605
2946518988@qq.com <2946518988@qq.com>
2019-09-14 16:06:45 +0800

    add config

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import './style/style.css';
 import './style/a.scss'
 
 function createDomElement() {
+  console.log('aaaaa')
   var dom = document.createElement('div');
   dom.innerHTML = _.join(['aicoder', '.com', ' wow'], '');
   dom.className = 'hello scssStyle';
   return dom;
+  
 }
 
 document.body.appendChild(createDomElement());
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,14 @@ module.exports = {
     filename: 'main[hash].js',
     path: path.resolve(__dirname, './dist')
   },
+  //js sourceMap 使用 inline-source-map 选项，这有助于解释说明 js 原始出错的位置。
+  //（不要用于生产环境）
+  devtool: 'inline-source-map',
+  //webpack-dev-server 为你提供了一个简单的 web 服务器
+  //并且能够实时重新加载(live reloading)
+  devServer: {
+    contentBase: './dist'
+  },
   module: {
     // 防止 webpack 解析那些任何与给定正则表达式相匹配的文件。
     //忽略的文件中不应该含有 import, require, define 的调用，或任何其他导入机制。
@@ -65,6 +73,56 @@ module.exports = {
               sourceMap: true
             }
           }
+        ]
+      },
+
+      //加载图片和图片优化
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          //加载图片，file-loader处理文件的导入
+          //'file-loader',
+
+          //url-loader功能类似于 file-loader，
+          //可以把 url 地址对应的文件，打包成 base64 的 DataURL，提高访问的效率。
+          {
+            loader: 'url-loader', // 根据图片大小，把图片优化成base64
+            options: {
+              limit: 10000
+            }
+          },
+
+          //图片优化，image-webpack-loader可以帮助我们对图片进行压缩和优化。
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65,0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ]
+      },
+
+      //字体处理，由于 css 中可能引用到自定义的字体，处理也是跟图片一致。
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
         ]
       }
     ]
